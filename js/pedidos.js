a858dcc7153a01e58f5c94e24b77e7bc250ce347
KaykyM18 <kayky12guerra@gmail.com>
2024-09-17 19:15:28 -0300

    add tela de pedidos

--- js/pedidos.js
+++ js/pedidos.js
@@ -1,3 +1,6 @@
-function finalizarPedido() {
-    alert('Pedido Finalizado!');
-}
+document.querySelector('.finalize-button').addEventListener('click', function() {
+    const order = this.closest('.order');
+    order.classList.remove('in-progress');
+    order.classList.add('finished');
+    this.style.display = 'none';  // Hide the button after clicking
+});
